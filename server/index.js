786e5d48c7fc8c3db887123b659e1859d0fa6cea
kellerchch <kellerchch@gmail.com>
2018-03-08 17:03:44 -0700

    updating

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const app = express();
+
+
+
+
+
+const port = process.env.PORT || 3000
+app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
